eec53fde464969377f4e6900ebaea6b2832c62f9
Cas Cremers <cas.cremers@gmail.com>
2015-12-07 11:27:25 +0000

    Fixing a typo in a header file.

--- src/abstraction/subtype.h
+++ src/abstraction/subtype.h
@@ -6,7 +6,7 @@
 int isSubtype (Term, Term);
 Term subTypeInSubterm (Term sttype, Term type);
 Term getTermType (Term);
-Term upperCommonSuptype (Term, Term);
+Term upperCommonSubtype (Term, Term);
 int isComparable (Term typ1, Term typ2);
 int compareTypelist (Term type, Termlist termlist);
 int compareTypeTermlist (Term type, Termlist termlist);
